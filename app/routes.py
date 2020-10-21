0410794bc71c731a37ce0a90c7720ae476591a6d
geer26 <gergo.kurucz@gmail.com>
2020-10-21 16:49:27 +0200

    v0.1

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    form = LoginForm
+
+    if request.method == 'POST':
+        print('what now?')
+        if form.validate_on_submit():
+            return redirect('/')
+
     user = {'username': 'geer26'}
     return render_template('index.html', title='Home', user=user)
