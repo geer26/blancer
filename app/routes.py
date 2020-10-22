0e9f126b0cd16df920e20da346c7c21703442e52
geer26 <gergo.kurucz@gmail.com>
2020-10-22 10:49:17 +0200

    v0.1

--- app/routes.py
+++ app/routes.py
@@ -19,3 +19,53 @@ def index():
 def logout():
     logout_user()
     return redirect('/')
+
+
+@socket.on('newmessage')
+def newmessage(data):
+
+    print(data)
+    """sid = request.sid
+
+    #incoming signup request
+    if data['event'] == 211:
+        #signup request
+        data['sid'] = sid
+        if verifiy_signup(data):
+            mess = {}
+            mess['event'] = 111
+            mess['htm'] = render_template('infomessage.html', message='You can login now!')
+            socket.emit('newmessage', mess, room=sid)
+
+        else:
+            socket.emit('newmessage', {'event': 119}, room=sid)
+            mess = {}
+            mess['event'] = 191
+            mess['htm'] = render_template('errormessage.html', message='SIGNUP NOT SUCCESS!')
+            socket.emit('newmessage', mess, room=sid)
+
+        return True
+
+    #incoming login request
+    if data['event'] == 221:
+        if verify_login(data):
+            mess = {}
+            mess['event'] = 121
+            mess['status'] = 1
+            socket.emit('newmessage', mess, room=sid)
+        else:
+            socket.emit('newmessage', {'event' : 129}, room=sid)
+            mess={}
+            mess['event'] = 191
+            mess['htm'] = render_template('errormessage.html', message='LOGIN NOT SUCCESS!')
+            socket.emit('newmessage', mess, room=sid)
+
+        return True
+
+    if data['event'] == 291:
+        mess = {}
+        mess['event'] = 191
+        mess['htm'] = render_template('errormessage.html', message=data['message'])
+        socket.emit('newmessage', mess, room=sid)"""
+
+    return True
