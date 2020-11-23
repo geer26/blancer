1d993477acc4e8845a8693d0b5c6b8c5fcd2bb45
geer26 <gergo.kurucz@gmail.com>
2020-11-23 08:40:33 +0100

    v0.4

--- app/static/js/sliderhandle.js
+++ app/static/js/sliderhandle.js
@@ -9,10 +9,26 @@ function createslider(minmax){
     var mintime = minmax['min'];
     var maxtime = minmax['max'];
 
+    mit = parseInt(mintime);
+    mat = parseInt(maxtime);
+
+    var mindate = {
+        year: new Date( mit ).getFullYear(),
+        month: new Date( mit ).getMonth() + 1,
+        day: new Date( mit ).getDate()
+        };
+
+    var maxdate = {
+        year: new Date( mat ).getFullYear(),
+        month: new Date( mat ).getMonth() + 1,
+        day: new Date( mat ).getDate()
+        };
+
     $('#sd').val(timestamp(mintime));
     $('#ed').val(timestamp(maxtime));
 
-    var slider = document.querySelector('#rangeselector');
+    document.getElementById("sd").min = mindate['year'] +'-'+mindate['month']+'-'+mindate['day'] ;
+    document.getElementById("ed").max = maxdate['year'] +'-'+maxdate['month']+'-'+maxdate['day'] ;
 
     var options = {
         start: [timestamp(mintime), timestamp(maxtime)],
@@ -25,9 +41,42 @@ function createslider(minmax){
 
     noUiSlider.create(slider, options);
 
+    var fromdate = {
+        year: new Date( parseInt(slider.noUiSlider.get()[0]) ).getFullYear(),
+        month: new Date( parseInt(slider.noUiSlider.get()[0]) ).getMonth() + 1,
+        day: new Date( parseInt(slider.noUiSlider.get()[0]) ).getDate()
+        };
+
+    var todate = {
+        year: new Date( parseInt(slider.noUiSlider.get()[1]) ).getFullYear(),
+        month: new Date( parseInt(slider.noUiSlider.get()[1]) ).getMonth() + 1,
+        day: new Date( parseInt(slider.noUiSlider.get()[1]) ).getDate()
+        };
+
+    $('#sd').val( fromdate['year'] +'-'+fromdate['month']+'-'+fromdate['day'] );
+    $('#ed').val( todate['year'] +'-'+todate['month']+'-'+todate['day'] );
+    document.getElementById("sd").max = document.getElementById("ed").value;
+    document.getElementById("ed").min = document.getElementById("sd").value;
+
 };
 
 
-function updslider(target){
-    console.log(target);
-}
+function updslider(){
+
+    var fromdate = {
+        year: new Date( parseInt(slider.noUiSlider.get()[0]) ).getFullYear(),
+        month: new Date( parseInt(slider.noUiSlider.get()[0]) ).getMonth() + 1,
+        day: new Date( parseInt(slider.noUiSlider.get()[0]) ).getDate()
+        };
+
+    var todate = {
+        year: new Date( parseInt(slider.noUiSlider.get()[1]) ).getFullYear(),
+        month: new Date( parseInt(slider.noUiSlider.get()[1]) ).getMonth() + 1,
+        day: new Date( parseInt(slider.noUiSlider.get()[1]) ).getDate()
+        };
+
+    $('#sd').val( fromdate['year'] +'-'+fromdate['month']+'-'+fromdate['day'] );
+    $('#ed').val( todate['year'] +'-'+todate['month']+'-'+todate['day'] );
+    document.getElementById("sd").max = document.getElementById("ed").value;
+    document.getElementById("ed").min = document.getElementById("sd").value;
+};
