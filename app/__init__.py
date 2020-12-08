2a1eef308b4c1ddc617d145192a1a89105c5ff8d
geer26 <gergo.kurucz@gmail.com>
2020-12-08 11:18:48 +0100

    v0.5

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from config import Config, SQLite, PostgreSQL
+from config import SQLite, PostgreSQL
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config.from_object(SQLite)
-#app.config.from_object(PostgreSQL)
-
-print(app.config['SENDGRID_API_KEY'])
+#app.config.from_object(SQLite)
+app.config.from_object(PostgreSQL)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-import os
 from os import environ, path
 from dotenv import load_dotenv
+
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
@@ -8,7 +8,7 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY') or '!01ChAnGeThIs!89'
+    SECRET_KEY = environ.get('SECRET_KEY') or '01!ChAnGeThIs!89'
     SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME') or 'Blancer2020'
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
